593e29cf6bee28d265f09a5d7d3422b68309cec0
Prateek Kumar <prateek@prateekkumar.in>
2017-10-22 22:18:01 +0530

    Wrote custom constraint parser
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- include/custom_parser.h
+++ include/custom_parser.h
@@ -2,47 +2,118 @@
 #define CUSTOM_PARSER_H
 
 #include <tao/pegtl.hpp>
+#include <string>
+#include <vector>
+#include "clauses.h"
 
 namespace pegtl = tao::TAOCPP_PEGTL_NAMESPACE;
 
 using namespace pegtl;
 
-struct integer : seq<range<'1', '9'>, star<digit>> {};
-struct instr : TAOCPP_PEGTL_KEYWORD("IN") {};
-struct notstr : TAOCPP_PEGTL_KEYWORD("NOT") {};
-struct andstr : TAOCPP_PEGTL_KEYWORD<"AND"> {};
-struct orstr : TAOCPP_PEGTL_KEYWORD<"OR"> {};
-
-struct fieldtype : identifier {};
-struct fieldvalues : list_must<identifier, one<','>, space> {};
-struct classvalues : list_must<identifier, one<','>, space> {};
-struct slotvalues : list_must<identifier, one<','>, space> {};
-struct classroomdecl : seq<pad<TAOCPP_PEGTL_KEYWORD("CLASSROOM"), space>, classvalues> {};
-struct slotdecl : seq<pad<TAOCPP_PEGTL_KEYWORD("SLOT"), space>, slotvalues> {};
-struct constraint_expr : seq<fieldtype, fieldvalues, opt<notstr>, instr, sor<classroomdecl, slotdecl>> {};
-
-struct constraint;
-struct constraint_braced : seq<opt<notstr>, if_must<one<'('>, pad<constraint, space>, one<')'>>> {};
-struct constraint_val : sor <constraint_expr, constraint_braced> {};
-struct constraint_and : list_must<constraint_val, andstr, space> {};
-struct constraint_or : list_must<constraint_and, orstr, space> {};
-
-struct wconstraint : seq<constraint_or, pad<TAOCPP_PEGTL_KEYWORD("WEIGHT"), space>, pad<integer, space>> {};
-struct grammar : plus<wconstraint> {};
-
 template <typename Rule>
 struct action : nothing<Rule> {};
 
 struct Object {
+    bool isNot;
+    bool takeFieldValues;
+    bool takeClassValues;
+    bool takeSlotValues;
+    int integer;
+    TimeTabler* timeTabler;
 
+    std::string fieldType;
+    std::vector<int> fieldValues;
+    std::vector<int> classValues;
+    std::vector<int> slotValues;
+
+    Clauses constraint;
+    std::vector<Clauses> constraintAnds;
+    std::vector<Clauses> constraintVals;    
 };
 
-template <>
-struct action<grammar> {
-    template <typename Input>
-    static void apply(const Input& in, Object &obj) {
-        std::out << in.string() << std::endl;
-    }
+struct integer : seq<range<'1', '9'>, star<digit>> {};
+template <> struct action<integer> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct instr : TAOCPP_PEGTL_KEYWORD("IN") {};
+template <> struct action<instr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct notstr : TAOCPP_PEGTL_KEYWORD("NOT") {};
+template <> struct action<notstr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct andstr : TAOCPP_PEGTL_KEYWORD("AND") {};
+template <> struct action<andstr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct orstr : TAOCPP_PEGTL_KEYWORD("OR") {};
+template <> struct action<orstr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct classroomstr : TAOCPP_PEGTL_KEYWORD("CLASSROOM") {};
+template <> struct action<classroomstr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct slotstr : TAOCPP_PEGTL_KEYWORD("SLOT") {};
+template <> struct action<slotstr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+
+struct fieldtype : identifier {};
+template <> struct action<fieldtype> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct value : plus<not_one<','>> {};
+template <> struct action<value> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct values : seq<pad<one<'{'>,space>,list<value,one<','>,space>,pad<one<'}'>,space>> {};
+template <> struct action<values> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct classroomdecl : seq<pad<classroomstr, space>, values> {};
+template <> struct action<classroomdecl> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct slotdecl : seq<pad<slotstr, space>, values> {};
+template <> struct action<slotdecl> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct constraint_expr : seq<pad<fieldtype,space>,values,opt<notstr>,pad<instr,space>,sor<classroomdecl,slotdecl>> {};
+template <> struct action<constraint_expr> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+
+struct constraint_or;
+struct constraint_braced : seq<opt<notstr>, if_must<pad<one<'('>,space>,constraint_or,pad<one<')'>,space>>> {};
+template <> struct action<constraint_braced> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct constraint_not : seq<pad<notstr, space>, constraint_braced> {};
+template <> struct action<constraint_not> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct constraint_val : sor<constraint_expr, constraint_not, constraint_braced> {};
+template <> struct action<constraint_val> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct constraint_and : list<constraint_val, andstr, space> {};
+template <> struct action<constraint_and> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct constraint_or : list<constraint_and, orstr, space> {};
+template <> struct action<constraint_or> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+
+struct wconstraint : seq<pad<constraint_or, space>, pad<TAOCPP_PEGTL_KEYWORD("WEIGHT"), space>, pad<integer, space>> {};
+template <> struct action<wconstraint> {
+    template <typename Input> static void apply(const Input&, Object &);
+};
+struct grammar : star<wconstraint> {};
+template <> struct action<constraint_expr> {
+    template <typename Input> static void apply(const Input&, Object &);
 };
 
 #endif
--- src/custom_parser.cpp
+++ src/custom_parser.cpp
@@ -1,169 +1,197 @@
-#ifndef CUSTOM_PARSER_H
-#define CUSTOM_PARSER_H
+#include "custom_parser.h"
 
-#include <tao/pegtl.hpp>
-#include <iostream>
-
-namespace pegtl = tao::TAOCPP_PEGTL_NAMESPACE;
-
-// using namespace pegtl;
-
-struct Object {
-
-};
-
-template <typename Rule>
-struct action : pegtl::nothing<Rule> {};
-
-struct integer : pegtl::seq<pegtl::range<'1', '9'>, pegtl::star<pegtl::digit>> {};
-template <>
-struct action<integer> {
+template <> struct action<integer> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "integer " << in.string() << std::endl;
+        obj.integer = std::stoi(in.string());
     }
 };
 
-struct instr : TAOCPP_PEGTL_KEYWORD("IN") {};
-template <>
-struct action<instr> {
+template <> struct action<instr> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<notstr> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "instr " << in.string() << std::endl;
+        obj.isNot = true;
     }
 };
 
-struct notstr : TAOCPP_PEGTL_KEYWORD("NOT") {};
-template <>
-struct action<notstr> {
+template <> struct action<andstr> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<orstr> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<classroomstr> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "notstr " << in.string() << std::endl;
+        obj.takeFieldValues = false;
+        obj.takeClassValues = true;
+        obj.taleSlotValues = false;
     }
 };
 
-struct andstr : TAOCPP_PEGTL_KEYWORD("AND") {};
-template <>
-struct action<andstr> {
+template <> struct action<slotstr> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "andstr " << in.string() << std::endl;
+        obj.takeFieldValues = false;
+        obj.takeClassValues = false;
+        obj.taleSlotValues = true;
     }
 };
 
-struct orstr : TAOCPP_PEGTL_KEYWORD("OR") {};
-template <>
-struct action<orstr> {
+template <> struct action<fieldtype> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "orstr " << in.string() << std::endl;
+        obj.fieldType = in.string();
+        obj.takeFieldValues = true;
+        obj.takeClassValues = false;
+        obj.taleSlotValues = false;
+        obj.isNot = false;
     }
 };
 
-struct classroomstr : TAOCPP_PEGTL_KEYWORD("CLASSROOM") {};
-template <>
-struct action<classroomstr> {
+template <> struct action<value> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "classroomstr " << in.string() << std::endl;
+        std::string val = in.string();
+        int index = -1;
+        if (obj.takeFieldValues) {
+            if (obj.fieldType == "INSTRUCTOR") {
+                for (int i=0; i<obj.timeTabler->data.instructors.size(); i++) {
+                    if (obj.timeTabler->data.instructors[i].getName() == val) {
+                        index = i;
+                        break;
+                    }
+                }
+            } else if (obj.fieldType == "COURSE") {
+                for (int i=0; i<obj.timeTabler->data.courses.size(); i++) {
+                    if (obj.timeTabler->data.courses[i].getName() == val) {
+                        index = i;
+                        break;
+                    }
+                }
+            } else if (obj.fieldType == "SEGMENT") {
+                for (int i=0; i<obj.timeTabler->data.segments.size(); i++) {
+                    if (obj.timeTabler->data.segments[i].toString() == val) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            obj.fieldValues.push_back(index);
+        } else if (obj.takeClassValues) {
+            for (int i=0; i<obj.timeTabler->data.classrooms.size(); i++) {
+                if (obj.timeTabler->data.classrooms[i].getNumber() == val) {
+                    index = i;
+                    break;
+                }
+            }
+            obj.classValues.push_back(index);
+        } else if (obj.takeSlotValues) {
+            for (int i=0; i<obj.timeTabler->data.slots.size(); i++) {
+                if (obj.timeTabler->data.slots[i].getNumber() == val) {
+                    index = i;
+                    break;
+                }
+            }
+            obj.slotValues.push_back(index);
+        }
     }
 };
 
-struct slotstr : TAOCPP_PEGTL_KEYWORD("SLOT") {};
-template <>
-struct action<slotstr> {
+template <> struct action<values> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<classroomdecl> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<slotdecl> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<constraint_expr> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "slotstr " << in.string() << std::endl;
+        Clauses clauses;
+        if (obj.fieldType == "INSTRUCTOR") {
+            
+        } else if (obj.fieldType == "COURSE") {
+
+        } else if (obj.fieldType == "SEGMENT") {
+            
+        }
+        obj.constraint = clauses;
+        obj.fieldValues.clear();
+        obj.classValues.clear();
+        obj.slotValues.clear();
     }
 };
 
-struct fieldtype : pegtl::identifier {};
-template <>
-struct action<fieldtype> {
+template <> struct action<constraint_braced> {
+    template <typename Input>
+    static void apply(const Input& in, Object &obj) {}
+};
+
+template <> struct action<constraint_not> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "fieldtype " << in.string() << std::endl;
+        Clauses clauses = obj.constraint;
+        obj.constraint = ~clauses;
     }
 };
 
-struct fieldvalues : pegtl::seq<pegtl::pad<pegtl::one<'{'>, pegtl::space>,
-    pegtl::list<pegtl::identifier, pegtl::one<','>, pegtl::space>,
-    pegtl::pad<pegtl::one<'{'>, pegtl::space>> {};
-template <>
-struct action<fieldvalues> {
+template <> struct action<constraint_val> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "fieldvalues " << in.string() << std::endl;
+        obj.constraintVals.push_back(obj.constraint);
     }
 };
 
-struct classvalues : pegtl::seq<pegtl::pad<pegtl::one<'{'>, pegtl::space>,
-    pegtl::list<pegtl::identifier, pegtl::one<','>, pegtl::space>,
-    pegtl::pad<pegtl::one<'{'>, pegtl::space>> {};
-template <>
-struct action<classvalues> {
+template <> struct action<constraint_and> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "classvalues " << in.string() << std::endl;
+        Clauses clauses = obj.constraintVals[0];
+        for (unsigned i = 1; i < obj.constraintVals.size(); i++) {
+            clauses = clauses & obj.constraintVals[i];
+        }
+        obj.constraintVals.clear();
+        obj.constraintAnds.push_back(clauses);
     }
 };
 
-struct slotvalues : pegtl::seq<pegtl::pad<pegtl::one<'{'>, pegtl::space>,
-    pegtl::list<pegtl::identifier, pegtl::one<','>, pegtl::space>,
-    pegtl::pad<pegtl::one<'{'>, pegtl::space>> {};
-template <>
-struct action<slotvalues> {
+template <> struct action<constraint_or> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "slotvalues " << in.string() << std::endl;
+        Clauses clauses = obj.constraintAnds[0];
+        for (unsigned i = 1; i < obj.constraintAnds.size(); i++) {
+            clauses = clauses | obj.constraintAnds[i];
+        }
+        obj.constraintAnds.clear();
+        obj.constraint = clauses;
     }
 };
 
-struct classroomdecl : pegtl::seq<pegtl::pad<classroomstr, pegtl::space>, classvalues> {};
-template <>
-struct action<classroomdecl> {
+template <> struct action<wconstraint> {
     template <typename Input>
     static void apply(const Input& in, Object &obj) {
-        std::cout << "classroomdecl " << in.string() << std::endl;
+        // TODO
     }
 };
 
-struct slotdecl : pegtl::seq<pegtl::pad<slotstr, pegtl::space>, slotvalues> {};
-template <>
-struct action<slotdecl> {
+template <> struct action<grammar> {
     template <typename Input>
-    static void apply(const Input& in, Object &obj) {
-        std::cout << "slotdecl " << in.string() << std::endl;
-    }
+    static void apply(const Input& in, Object &obj) {}
 };
-
-struct constraint_expr : pegtl::seq<fieldtype, pegtl::star<pegtl::space>, fieldvalues, pegtl::star<pegtl::space>,
-    pegtl::opt<notstr>, pegtl::star<pegtl::space>, instr, pegtl::star<pegtl::space>,
-    pegtl::sor<classroomdecl, slotdecl>> {};
-template <>
-struct action<constraint_expr> {
-    template <typename Input>
-    static void apply(const Input& in, Object &obj) {
-        std::cout << "constraint_expr " << in.string() << std::endl;
-    }
-};
-
-struct constraint_or;
-struct constraint_braced : pegtl::seq<pegtl::opt<notstr>, pegtl::if_must<pegtl::one<'('>, pegtl::pad<constraint_or, pegtl::space>, pegtl::one<')'>>> {};
-struct constraint_val : pegtl::sor <constraint_expr, constraint_braced> {};
-struct constraint_and : pegtl::list<constraint_val, andstr, pegtl::space> {};
-struct constraint_or : pegtl::list<constraint_and, orstr, pegtl::space> {};
-
-struct wconstraint : pegtl::seq<constraint_or, pegtl::star<pegtl::space>, pegtl::pad<TAOCPP_PEGTL_KEYWORD("WEIGHT"), pegtl::space>, pegtl::pad<integer, pegtl::space>> {};
-struct grammar : pegtl::plus<wconstraint> {};
-
-
-int main(int argc, char const *argv[]) {
-    pegtl::file_input<> in("test.txt");
-    Object obj;
-    pegtl::parse<grammar, action>(in, obj);
-    return 0;
-}
-
-#endif
