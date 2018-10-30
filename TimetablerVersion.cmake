875b7bc8d7a6ed401c68f4200e35c61919d61a58
Prateek Kumar <prateek@prateekkumar.in>
2018-10-31 03:14:00 +0530

    Move version number to TimetablerVersion.cmake
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

---
+++ TimetablerVersion.cmake
@@ -0,0 +1,6 @@
+set(Timetabler_VERSION_MAJOR 0)
+set(Timetabler_VERSION_MINOR 3)
+set(Timetabler_VERSION_PATCH 0)
+# Set to "" for tagged release and "+git" for others
+set(Timetabler_VERSION_SUFFIX "+git")
+set(Timetabler_VERSION ${Timetabler_VERSION_MAJOR}.${Timetabler_VERSION_MINOR}.${Timetabler_VERSION_PATCH}${Timetabler_VERSION_SUFFIX})
