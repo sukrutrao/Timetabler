9c552fe3e6e07caf47db813491f8f4fb34978b8b
Prateek Kumar <prateek@prateekkumar.in>
2018-12-12 13:09:11 +0530

    Run gen_version.sh inside cmake
    Signed-off-by: Prateek Kumar <prateek@prateekkumar.in>

--- gen_version.sh
+++ gen_version.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 Timetabler_VERSION=$(git describe --always --tags --dirty)
+if [ -z "$Timetabler_VERSION" ]
+then
+	# Update the below line after every release
+	Timetabler_VERSION="v0.3.0"
+fi
 VERSION_INCLUDE_STR="#ifndef __TIMETABLER_VERSION__\n#define __TIMETABLER_VERSION__ \"${Timetabler_VERSION}\"\n#endif"
 echo $Timetabler_VERSION
 echo -e $VERSION_INCLUDE_STR > $1/include/version.h
-echo "set (Timetabler_VERSION \"${Timetabler_VERSION}\")" > $1/TimetablerVersion.cmake
+echo "set (Timetabler_VERSION \"${Timetabler_VERSION:1}\")" > $1/TimetablerVersion.cmake
